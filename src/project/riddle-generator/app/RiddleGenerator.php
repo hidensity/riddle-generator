ae9ce13a97b8cb45c3062f9008187c1da3bf233e
dennis <hidensity@gmx.net>
2016-09-28 20:01:10 +0200

    Removed dependency to Silex

--- src/project/riddle-generator/app/RiddleGenerator.php
+++ src/project/riddle-generator/app/RiddleGenerator.php
@@ -4,8 +4,39 @@ namespace Dbb;
 
 class RiddleGenerator
 {
+    /**
+     * @var array
+     */
+    protected $alphabet = [];
+
+    /**
+     * @var array
+     */
+    protected $shuffledIndices = [];
+
+    public function __construct()
+    {
+        $this->setAlphabet(range('A', 'Z'));
+    }
+
     public function run()
     {
-        echo 'ok';
+        phpinfo();
+    }
+
+    /**
+     * @param array $alphabet
+     * @return $this
+     */
+    public function setAlphabet(array $alphabet)
+    {
+        $this->alphabet = $alphabet;
+        $this->shuffleIndices();
+        return $this;
+    }
+
+    protected function shuffleIndices()
+    {
+        $this->shuffledIndices = range(1, count($this->alphabet));
     }
 }
